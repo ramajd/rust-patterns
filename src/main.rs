27c081298f9331c1320d9f784f2a040a3269d8b1
Reza Alizadeh Majd <r.a.majd@gmail.com>
2021-01-07 22:46:12 +0330

    update main app to run patterns by commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::env;
+
 mod implementations;
 mod patterns;
 
 use crate::implementations::run_observer_logic;
+use crate::implementations::run_singleton_pattern;
 
 fn main() {
-    run_observer_logic();
+    let pattern = env::args().nth(1).expect("pattern name is not specified!");
+
+    match pattern.as_str() {
+        "singleton" => run_singleton_pattern(),
+        "observer" => run_observer_logic(),
+        _ => println!("Error: Invalid pattern"),
+    }
 }
